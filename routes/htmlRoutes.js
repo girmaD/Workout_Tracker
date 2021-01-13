3e81028a062cc6f099f5385a9d6b618da777fa01
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-12 17:37:28 -0800

    uses router for routes to html files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,18 @@
+const router = require('express').Router()
 const path = require('path');
 
-module.exports = app => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-    app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
-    app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-}
+////////////////////////////
+//// routes to html files //
+///////////////////////////
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+//export router to make it accessible to other files
+module.exports = router;
