adf493d5ae4b8015d07d7350ee6817288312ab8c
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-14 13:07:17 -0800

    fixed issues with mongoose schema

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    Workout: require("./workout"),
-    Exercise: require("./excercise")
+    Workout: require("./workout")
+    
 }
--- models/workout.js
+++ models/workout.js
@@ -7,12 +7,41 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    excercises: [
+    exercise: [
         {
-            type: Schema.Types.ObjectId,
-            ref: 'Exercise'
+            type: {
+                type: String,
+                trim: true,
+                required: 'Please enter the type of the exercise'
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Please enter the name of the exercise'
+            },
+            duration: {
+                type: Number,        
+                required: 'Please enter the duration of the exercise'
+            },
+            weight: {
+                type: Number,
+                default: 0        
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0       
+            }
         }
-    ]
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
+    
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
