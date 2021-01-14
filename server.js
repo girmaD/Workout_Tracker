998400ca07e70b90510c6545391b9cca24110a31
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-14 15:47:41 -0800

    includes mongoose connect

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 //require modules
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 
 app.use(logger('dev'));
 
@@ -13,8 +20,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+
+
 //html routes
-app.use(require('./routes/htmlRoutes'))
+app.use(require('./routes/htmlRoutes'));
+app.use(require('./routes/apiRoutes'));
 
 app.listen(PORT, ()=>{
     console.log('this app is listening to port', PORT)
