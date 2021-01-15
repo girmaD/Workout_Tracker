3b2472dc4e0adcd5afa77ce4868903b263291fc2
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-15 15:07:16 -0800

    adds comments explaining codes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+//set port variable for remote and local
 const PORT = process.env.PORT || 3000;
 
+// declate app to be express invoked as a function
 const app = express();
 
+// mongoose connet either to remote env variable for deployment or to local db called workout
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
@@ -17,20 +20,23 @@ mongoose.connect(
   }
 );
 
-
+// app use logger(morgan) to log data
 app.use(logger('dev'));
 
+// parsing req.body coming from the frontend
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// use startic filed in the public folder
 app.use(express.static('public'));
 
 
 
-//html routes
+//html routes adn apiRoutes
 app.use(require('./routes/htmlRoutes'));
 app.use(require('./routes/apiRoutes'));
 
+// listening to port
 app.listen(PORT, ()=>{
     console.log('this app is listening to port', PORT)
 })
